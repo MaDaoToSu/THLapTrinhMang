ef07f81a953749789e687dd9e033ef4dd5f0d6e4
011223345aA <huyhoang11202@gmail.com>
2023-07-04 14:17:37 +0700

    first commit

---
+++ ThucHanh1/src/thuchanh1/MyConnection.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thuchanh1;
+
+import java.sql.*;
+
+/**
+ *
+ * @author huyho
+ */
+public class MyConnection {
+    Connection conn = null;
+    public Connection getConnect() {
+        String database = "quanlytaikhoan";
+        String username ="";
+        String password = "";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://127.0.0.1:9999;" + "databaseName=" + database + ";user=" + username + ";password=" + password;
+            System.out.println(url);
+            conn = DriverManager.getConnection(url);
+            System.out.println("Da ket noi voi database " + conn.getCatalog());
+            return conn;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ ThucHanh1/src/thuchanh1/ThucHanh1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package thuchanh1;
+
+/**
+ *
+ * @author huyho
+ */
+public class ThucHanh1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMain main = new frmMain();
+        main.setVisible(true);
+    }
+    
+}
